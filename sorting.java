06dd3b314b28058666af0df37454841843b844cb
Akash chaudhary <Akash.chaudhary@codeaegis.in>
2022-01-24 17:27:01 +0530

    c

--- sorting.java
+++ sorting.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 import java.util.Collections;
 public class sorting{
-  static int[] arr = { 13, 7, 6, 45, 21, 9, 100, 100 };
+  static Integer[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
   public static void main(String[] args) {
-    Arrays.sort(arr,Collections.reverseOrder());
+    Arrays.sort(arr, Collections.reverseOrder());
     for (int i = 0; i < arr.length; i++){
       System.out.println(arr[i]);
       }
