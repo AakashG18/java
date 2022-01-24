5749075f3e84ae5b0cb9228338cff32e651dd758
AAKASH  GUPTA <akash.gupta@codeaegis.in>
2022-01-24 17:14:46 +0530

    k

--- sorting.java
+++ sorting.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
 public class sorting{
-  static int[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
+  static int[] arr = { 13, 7, 6, 45, 21, 9, 100, 100 };
   public static void main(String[] args) {
     Arrays.sort(arr);
     for (int i = 0; i < arr.length; i++){
       System.out.println(arr[i]);
+
       }
+      System.out.println(arr[-2]);
   }
 }
