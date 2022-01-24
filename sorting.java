15818632de84cc16d975dc5a9e36bc3c1d7ff0f1
Akash chaudhary <Akash.chaudhary@codeaegis.in>
2022-01-24 16:55:09 +0530

    arrays library added

--- sorting.java
+++ sorting.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 public class sorting{
   static int[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
   public static void main(String[] args) {
     Arrays.sort(arr);
-    for (int i = 0; i < arr.length; i++){       
+    for (int i = 0; i < arr.length; i++){
       System.out.println(arr[i]);
       }
   }
