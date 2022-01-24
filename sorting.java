de675b448a6e86899a9d86260ffa5e19c72eb508
Akash chaudhary <Akash.chaudhary@codeaegis.in>
2022-01-24 16:53:21 +0530

    sorting.java sorting algo added

--- sorting.java
+++ sorting.java
@@ -1,5 +1,9 @@
 public class sorting{
+  static int[] arr = { 13, 7, 6, 45, 21, 9, 2, 100 };
   public static void main(String[] args) {
-    System.out.println("tests");
+    Arrays.sort(arr);
+    for (int i = 0; i < arr.length; i++){       
+      System.out.println(arr[i]);
+      }
   }
 }
